dd03c2a6079c37f9e1a1a1153b3f0798bdef141e
LingniShao <47163844+LingniShao@users.noreply.github.com>
2019-02-12 14:41:14 +0000

    Add files via upload

--- SRK_Assign2_monte_carlo.R
+++ SRK_Assign2_monte_carlo.R
@@ -14,24 +14,24 @@ simu_norm <- function(n){
   # using kernel density and maximum penalized likelihood and print out integrated
   # squared error for both methods.
   
-  y <- rnorm(n,2,3)  # simulated data from Normal(2,3).
-  low <- min(y)
-  upp <- max(y)
+  x <- rnorm(n,2,3)  # simulated data from Normal(2,3).
+  low <- min(x)
+  upp <- max(x)
   
   # Get Kernel density estimate, with bandwidth as nrd0 and kernel as gaussian, 
   # since the data are symmetric.
-  fKde <- kdensity(y,bw='nrd0',kernel='gaussian',support=c(low,upp))
+  fKde <- kdensity(x,bw='nrd0',kernel='gaussian',support=c(low,upp))
   # Get maximum penalized likelihood estimate. Theoretically, the true domain for 
   # the density is unknown, so set the domain based on the data simulated.
-  fMple <- ssden(~y,domain=data.frame(y=c(low,upp)))
+  fMple <- ssden(~x,domain=data.frame(x=c(low,upp)))
   
   # Plot the true density and estimated densities, the domain is set based on the
   # data we simulated as well.
-  y1 <- seq(low,upp,len=500)
-  plot(y1, dnorm(y1, 2, 3),type = "l", lwd = 2, ylim = c(0,0.16), xlim=c(low,upp),
-       ylab = 'p.d.f',xlab = 'y',bty = 'n')  # Plot the true density.
-  lines(y1, fKde(y1), lwd = 1, col = 'green')  # Plot kernel density estimate.
-  lines(y1, dssden(fMple, y1), lwd = 1, col = 'red')  # Plot maximum penalized likelihood estimate
+  x1 <- seq(low,upp,len=500)
+  plot(x1, dnorm(x1, 2, 3),type = "l", lwd = 2, ylim = c(0,0.16), xlim=c(low,upp),
+       ylab = 'p.d.f',xlab = 'x from Normal(2,3)',bty = 'n')  # Plot the true density.
+  lines(x1, fKde(x1), lwd = 1, col = 'green')  # Plot kernel density estimate.
+  lines(x1, dssden(fMple, x1), lwd = 1, col = 'red')  # Plot maximum penalized likelihood estimate
   legend(2,0.165,c("True density function","Kernel density","Maximum penalized likelihood"), 
          col = c("black","green","red"), lty=1, cex = 0.3, box.lty=0)
   
@@ -49,24 +49,24 @@ simu_exp <- function(n){
   # using kernel density and maximum penalized likelihood and print out integrated
   # squared error for both methods.
   
-  y <- rexp(n,2)  # Simulated data from exponential(2)
-  low <- min(y)
-  upp <- max(y)
+  x <- rexp(n,2)  # Simulated data from exponential(2)
+  low <- min(x)
+  upp <- max(x)
   
   # Get Kernel density estimate, with bandwidth as ucv and kernel as gamma, since 
   # the data are asymmetric, and the support of gamma kernel covers the range of simulated data.
-  fKde <- kdensity(y,bw='ucv',kernel='gamma',support=c(low,upp))
+  fKde <- kdensity(x,bw='ucv',kernel='gamma',support=c(low,upp))
   # Get maximum penalized likelihood estimate. Theoretically, the true domain for 
   # the density is unknown, so set the domain based on the data simulated.
-  fMple <- ssden(~y,domain=data.frame(y=c(low,upp)))
+  fMple <- ssden(~x,domain=data.frame(x=c(low,upp)))
   
   # Plot the true density and estimated densities, the domain is set based on the
   # data we simulated as well.
-  y1 <- seq(low,upp,len=500)
-  plot(y1, dexp(y1, 2),type="l", lwd=2, ylim=c(0,2.5), xlim=c(low,upp), ylab = 'p.d.f',
-       xlab = 'y', bty='n')  # Plot the true density.
-  lines(y1, fKde(y1), lwd = 1, col = 'green')  # Plot kernel density estimate.
-  lines(y1, dssden(fMple,y1), lwd = 1, col = 'red')  # Plot maximum penalized likelihood estimate.
+  x1 <- seq(low,upp,len=500)
+  plot(x1, dexp(x1, 2),type="l", lwd=2, ylim=c(0,2.5), xlim=c(low,upp), ylab = 'p.d.f',
+       xlab = 'x from Exponential(2)', bty='n')  # Plot the true density.
+  lines(x1, fKde(x1), lwd = 1, col = 'green')  # Plot kernel density estimate.
+  lines(x1, dssden(fMple,x1), lwd = 1, col = 'red')  # Plot maximum penalized likelihood estimate.
   legend(1.5,2,c("True density function","Kernel density","Maximum penalized likelihood"), 
          col = c("black","green","red"), lty=1, cex = 0.3, box.lty=0)
   
@@ -84,24 +84,24 @@ simu_beta <- function(n){
   # using kernel density and maximum penalized likelihood and print out integrated
   # squared error for both methods.
   
-  y <- rbeta(n,3,1)  # Simulated data from beta(3,1).
-  low <- min(y)
-  upp <- max(y)
+  x <- rbeta(n,3,1)  # Simulated data from beta(3,1).
+  low <- min(x)
+  upp <- max(x)
   
   # Get Kernel density estimate, with bandwidth as ucv and kernel as beta, since 
   # the data are asymmetric, and the support of beta kernel covers the range of simulated data.
-  fKde <- kdensity(y,bw='ucv',kernel='beta',support=c(low,upp))
+  fKde <- kdensity(x,bw='ucv',kernel='beta',support=c(low,upp))
   # Get maximum penalized likelihood estimate. Theoretically, the true domain for
   # the density is unknown, so set the domain based on the data simulated.
-  fMple <- ssden(~y,domain=data.frame(y=c(low,upp)))
+  fMple <- ssden(~x,domain=data.frame(x=c(low,upp)))
   
   # Plot the true density and estimated densities, the domain is set based on the
   # data we simulated as well.
-  y1 <- seq(low,upp,len=500)
-  plot(y1, dbeta(y1,3,1),type="l", lwd=2, ylim=c(0,4), xlim=c(low,upp), ylab = 'p.d.f',
-       xlab = 'y', bty='n')  # Plot the true density.
-  lines(y1, fKde(y1), lwd = 1, col = 'green')  # Plot kernel density estimate.
-  lines(y1, dssden(fMple, y1), lwd = 1, col = 'red')   # Plot maximum penalized likelihood estimate.
+  x1 <- seq(low,upp,len=500)
+  plot(x1, dbeta(x1,3,1),type="l", lwd=2, ylim=c(0,4), xlim=c(low,upp), ylab = 'p.d.f',
+       xlab = 'x from Beta(3,1)', bty='n')  # Plot the true density.
+  lines(x1, fKde(x1), lwd = 1, col = 'green')  # Plot kernel density estimate.
+  lines(x1, dssden(fMple, x1), lwd = 1, col = 'red')   # Plot maximum penalized likelihood estimate.
   legend(0.2,3.5,c("True density function","Kernel density","Maximum penalized likelihood"), 
          col = c("black","green","red"), lty=1, cex = 0.3, box.lty=0)
   
--- SRK_Assign2_one-shot.R
+++ SRK_Assign2_one-shot.R
@@ -12,52 +12,52 @@ require(gss)
 ## Simulate data from normal(2,3), exponential(10) and beta(3,1)
 set.seed(1)
 n <- 100
-y1 <- rnorm(n,2,3)
-y2 <- rexp(n,2)
-y3 <- rbeta(n,3,1)
+x1 <- rnorm(n,2,3)
+x2 <- rexp(n,2)
+x3 <- rbeta(n,3,1)
 
 ## Calculate the kernel density estimates
 # Choose bandwidth as nrd0 and kernel as gaussian, since the data are symmetric.
-f1Kde <- kdensity(y1,bw='nrd0',kernel='gaussian', support=c(range(y1)[1],range(y1)[2]))
+f1Kde <- kdensity(x1,bw='nrd0',kernel='gaussian', support=c(range(x1)[1],range(x1)[2]))
 # Choose bandwidth as ucv and kernel as gamma, since the data are asymmetric and
 # the support of gamma kernel covers the range of simulated data.
-f2Kde <- kdensity(y2,bw='ucv',kernel='gamma', support=c(range(y2)[1],range(y2)[2]))
+f2Kde <- kdensity(x2,bw='ucv',kernel='gamma', support=c(range(x2)[1],range(x2)[2]))
 # Choose bandwidth as ucv and kernel as beta since the data are asymmetric and
 # the support of beta kernel covers the range of simulated data.
-f3Kde <- kdensity(y3,bw='ucv',kernel='beta',support=c(range(y3)[1],range(y3)[2]))
+f3Kde <- kdensity(x3,bw='ucv',kernel='beta',support=c(range(x3)[1],range(x3)[2]))
 
 ## Calculate the maximum penalized likelihood density estimates; theoretically, 
 ## the true domain for the density is unknown, so set the domain based on the data simulated.
-f1Mple <- ssden(~y1,domain=data.frame(y1=c(range(y1)[1],range(y1)[2])))
-f2Mple <- ssden(~y2,domain=data.frame(y2=c(range(y2)[1],range(y2)[2])))
-f3Mple <- ssden(~y3,domain=data.frame(y3=c(range(y3)[1],range(y3)[2])))
+f1Mple <- ssden(~x1,domain=data.frame(x1=c(range(x1)[1],range(x1)[2])))
+f2Mple <- ssden(~x2,domain=data.frame(x2=c(range(x2)[1],range(x2)[2])))
+f3Mple <- ssden(~x3,domain=data.frame(x3=c(range(x3)[1],range(x3)[2])))
 
 ## Plot the true density function and estimated density function, the domain is 
 ## set based on the data we simulated as well.
 # Plot for Normal(2,3) density and estimated densities.
-yy1 <- seq(range(y1)[1],range(y1)[2],len=100)
-plot(yy1, dnorm(yy1, 2, 3),type = "l", lwd = 2, ylim = c(0,0.16), xlim=c(range(y1)[1],range(y1)[2]),
-     ylab = 'p.d.f',xlab = 'y1',bty = 'n')  # Plot the true density.
-lines(yy1, f1Kde(yy1), lwd = 1, col = 'green')  # Plot kernel density estimate.
-lines(yy1, dssden(f1Mple, yy1), lwd = 1, col = 'red')  # Plot maximum penalized likelihood estimate
-legend(3.5,0.17,c("True density function","Kernel densit","Maximum penalized likelihood"), 
+xx1 <- seq(range(x1)[1],range(x1)[2],len=100)
+plot(xx1, dnorm(xx1, 2, 3),type = "l", lwd = 2, ylim = c(0,0.16), xlim=c(range(x1)[1],range(x1)[2]),
+     ylab = 'p.d.f',xlab = 'x1 from Normal(2,3)',bty = 'n')  # Plot the true density.
+lines(xx1, f1Kde(xx1), lwd = 1, col = 'green')  # Plot kernel density estimate.
+lines(xx1, dssden(f1Mple, xx1), lwd = 1, col = 'red')  # Plot maximum penalized likelihood estimate
+legend(3.5,0.17,c("True density function","Kernel density","Maximum penalized likelihood"), 
        col = c("black","green","red"), lty=1, cex = 0.3, box.lty=0)
 
 # Plot for Exponential(2) density and estimated densities.
-yy2 <- seq(range(y2)[1],range(y2)[2],len=100)
-plot(yy2, dexp(yy2, 2),type="l", lwd=2, ylim=c(0,2.2), xlim=c(range(y2)[1],range(y2)[2]), ylab = 'p.d.f',
-     xlab = 'y2', bty='n')  # Plot the true density.
-lines(yy2, f2Kde(yy2), lwd = 1, col = 'green')  # Plot kernel density estimate.
-lines(yy2, dssden(f2Mple,yy2), lwd = 1, col = 'red')  # Plot maximum penalized likelihood estimate.
+xx2 <- seq(range(x2)[1],range(x2)[2],len=100)
+plot(xx2, dexp(xx2, 2),type="l", lwd=2, ylim=c(0,2.2), xlim=c(range(x2)[1],range(x2)[2]), ylab = 'p.d.f',
+     xlab = 'x2 from Expnential(2)', bty='n')  # Plot the true density.
+lines(xx2, f2Kde(xx2), lwd = 1, col = 'green')  # Plot kernel density estimate.
+lines(xx2, dssden(f2Mple,xx2), lwd = 1, col = 'red')  # Plot maximum penalized likelihood estimate.
 legend(0.8,2,c("True density function","Kernel density","Maximum penalized likelihood"), 
        col = c("black","green","red"), lty=1, cex = 0.3, box.lty=0)
 
 # Plot for Beta(3,1) density and estimated densities.
-yy3 <- seq(range(y3)[1],range(y3)[2],len=100)
-plot(yy3, dbeta(yy3,3,1),type="l", lwd=2, ylim=c(0,4), xlim=c(range(y3)[1],range(y3)[2]), ylab = 'p.d.f',
-     xlab = 'y3', bty='n')  # Plot the true density.
-lines(yy3, f3Kde(yy3), lwd = 1, col = 'green')  # Plot kernel density estimate.
-lines(yy3, dssden(f3Mple, yy3), lwd = 1, col = 'red')  # Plot maximum penalized likelihood estimate.
+xx3 <- seq(range(x3)[1],range(x3)[2],len=100)
+plot(xx3, dbeta(xx3,3,1),type="l", lwd=2, ylim=c(0,4), xlim=c(range(x3)[1],range(x3)[2]), ylab = 'p.d.f',
+     xlab = 'x3 from Beta(3,1)', bty='n')  # Plot the true density.
+lines(xx3, f3Kde(xx3), lwd = 1, col = 'green')  # Plot kernel density estimate.
+lines(xx3, dssden(f3Mple, xx3), lwd = 1, col = 'red')  # Plot maximum penalized likelihood estimate.
 legend(0.2,3.5,c("True density function","Kernel density","Maximum penalized likelihood"), 
        col = c("black","green","red"), lty=1, cex = 0.3, box.lty=0)
 
